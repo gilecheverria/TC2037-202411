21225c96ccd48a81c5bc17bf48cb37835d8ae648
Gilberto Echeverria <gilbertoef@gmail.com>
2024-05-16 10:26:12 -0600

    Add documentation and labels to IO.inspect

--- Elixir/07_file_io.exs
+++ Elixir/07_file_io.exs
@@ -6,6 +6,17 @@
 
 defmodule Tfile do
 
+  @doc """
+  Function to extract all the emails found in a text file.
+  Will scan every line individually.
+  Arguments:
+  - in_filename : the name of the file to read
+  - out_filename : the name of a new file where emails will be stores, one in each line
+  Returns:
+  - :ok
+  - As a side effect, a new file is created
+  """
+  @spec get_emails(String.t(), String.t()) :: :ok
   def get_emails(in_filename, out_filename) do
 
     # Prepare the file to write the results
@@ -16,20 +27,36 @@ defmodule Tfile do
     # Open the file to read it line by line
     |> File.stream!()
     # Scan each line to find email addresses
-    |> Enum.map(&Regex.scan(~r/\w+@\w+\.\w{2,4}/, &1))
+    #|> Enum.map(&Regex.scan(~r/\w+@\w+\.\w{2,4}/, &1))
     # Another syntax for the anonymous function to the map
     #|> Enum.map(fn line -> Regex.scan(~r/\w+@\w+\.\w{2,4}/, line) end)
+    # Using a named function instead
+    |> Enum.map(&find_email(&1))
+    |> IO.inspect(label: "After scan")
     # Eliminate lines without an email
     |> Enum.filter(&(&1 != []))
-    |> IO.inspect()
+    |> IO.inspect(label: "After filter")
     # Remove the lists containing the emails
     |> Enum.map(&hd/1)
     |> Enum.map(&hd/1)
-    |> IO.inspect()
+    |> IO.inspect(label: "After getting the heads of the lists")
     # Write each line to the file
     |> Enum.map(&IO.puts(out_fd, &1))
 
     File.close(out_fd)
+  end
+
+  @doc """
+  Function to look for an email address inside a string
+  Arguments:
+  - line : a string
+  Returns:
+  - A list, either empty if no matches were found, or containing lists of matches
+  """
+  @spec find_email(String.t()) :: List
+  def find_email(line) do
+    IO.inspect(line, label: "Processing line")
+    Regex.scan(~r/\w+@\w+\.\w{2,4}/, line)
   end
 
 end
